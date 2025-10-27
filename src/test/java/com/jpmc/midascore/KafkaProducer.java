d8c631fa8b9cfbd540e3bade54b4eb0ed354de43
Kaushik Kumar Daga <kaushikdaga05@gmail.com>
2025-10-27 20:37:14 +0530

    chages mad for task 2

--- src/test/java/com/jpmc/midascore/KafkaProducer.java
+++ src/test/java/com/jpmc/midascore/KafkaProducer.java
@@ -1,22 +1,37 @@
 package com.jpmc.midascore;
 
-import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.foundation.Transaction; // Keep this import if needed elsewhere, but not for send method parameter
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaProducer {
-    private final String topic;
-    private final KafkaTemplate<String, Transaction> kafkaTemplate;
+    private static final Logger logger = LoggerFactory.getLogger(KafkaProducer.class);
 
-    public KafkaProducer(@Value("${general.kafka-topic}") String topic, KafkaTemplate<String, Transaction> kafkaTemplate) {
-        this.topic = topic;
-        this.kafkaTemplate = kafkaTemplate;
+    @Value("${general.kafka-topic}")
+    private String topicName;
+
+    // --- CHANGE KafkaTemplate TO <String, String> ---
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate; 
+    // ----------------------------------------------
+
+    // This method is correctly called by TaskTwoTests
+    public void send(String message) {
+        logger.info(String.format("#### -> Producing message -> %s", message));
+        // Now sends String using a KafkaTemplate specifically for Strings
+        this.kafkaTemplate.send(topicName, message); 
     }
 
-    public void send(String transactionLine) {
-        String[] transactionData = transactionLine.split(", ");
-        kafkaTemplate.send(topic, new Transaction(Long.parseLong(transactionData[0]), Long.parseLong(transactionData[1]), Float.parseFloat(transactionData[2])));
-    }
+    // --- REMOVE OR COMMENT OUT this method ---
+    // It causes confusion and is not used by TaskTwoTests
+    // public void send(Transaction transaction) { 
+    //     logger.info(String.format("#### -> Producing transaction -> %s", transaction.toString()));
+    //     this.kafkaTemplate.send(topicName, transaction); // This was likely causing the error indirectly
+    // }
+    // ----------------------------------------
 }
